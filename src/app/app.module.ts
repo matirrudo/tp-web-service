ce689ec420d4d0c7399052f7034f3b2e39ad4946
Matias Quiroga <matirrudo@gmail.com>
2020-06-05 03:20:00 -0300

    Actualizacion de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CotizacionComponent } from './component/cotizacion/cotizacion.component';
+import { NoticiasComponent } from './component/noticias/noticias.component';
+import { HoroscopoComponent } from './component/horoscopo/horoscopo.component';
+import { CovidComponent } from './component/covid/covid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    CotizacionComponent,
+    NoticiasComponent,
+    HoroscopoComponent,
+    CovidComponent
   ],
   imports: [
     BrowserModule,
